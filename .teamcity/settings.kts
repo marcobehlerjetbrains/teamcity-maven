d20db3cca995bc0c095647422e66d1a9f553d459
marco.behler@jetbrains.com <marco.behler@jetbrains.com@TeamCity>
2021-04-16 12:03:07 +0000

    TeamCity change in 'My Maven Project' project: parameters of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -42,6 +42,10 @@ project {
 object Build : BuildType({
     name = "Build"
 
+    params {
+        param("DockerImagePostfix", "")
+    }
+
     vcs {
         root(DslContext.settingsRoot)
     }
